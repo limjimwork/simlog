03cecfb1b9181054e0349a234af214d088108892
simpson <hisimpson90@gmail.com>
2020-05-28 14:44:00 +0900

    Set dotenv.

--- client/components/information.tsx
+++ client/components/information.tsx
@@ -5,12 +5,12 @@ export default function Information() {
     <Info.Wrap>
       <div className="info-photo" />
       <div className="info-text">
-        <h2>Lim Simpson</h2>
-        <p className="job">Front-end developer</p>
-        <p className="skill">React, Typescript,Javascript, Html5, Css3</p>
+        <h2>{process.env.name}</h2>
+        <p className="job">{process.env.job}</p>
+        <p className="skill">{process.env.skills}</p>
         <p className="link">
-          <a href="https://github.com/Limsimpson" target="_blank">
-            https://github.com/Limsimpson
+          <a href={process.env.link} target="_blank">
+            {process.env.link}
           </a>
         </p>
       </div>
